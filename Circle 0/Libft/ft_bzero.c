bb44c7b8620a4b33031fede4b769a51b4f199686
Hai Anh Le <hle@a2-r5-p1.s19.be>
2023-01-13 15:26:30 +0100

    Add Mandatory part

--- Circle 0/Libft/ft_bzero.c
+++ Circle 0/Libft/ft_bzero.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hle <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/13 14:42:15 by hle               #+#    #+#             */
+/*   Updated: 2023/01/13 15:19:01 by hle              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //#include "libft.h"
 #include "stdio.h"
 #include "string.h"
+//cannot work with *s directly
 
 void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
-	char	*str;
-
-	//cannot work with *s directly
+	char		*str;
+	
 	i = 0;
 	str = s;
 	while (i < n)
--- Circle 0/Libft/ft_memmove.c
+++ Circle 0/Libft/ft_memmove.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hle <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/13 14:44:23 by hle               #+#    #+#             */
+/*   Updated: 2023/01/13 15:13:12 by hle              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //#include "libft.h"
 #include <string.h>
 #include <stdio.h>
@@ -7,19 +19,18 @@ void	*ft_memove(void *dst, const void *src, size_t len)
 
 }
 */
-int	main ()
+/*
+int	main(void)
 {
-  char	str1[50] = "Geeks"; 
-  char	str2[50] = "Quiz"; 
- 
-  puts("str1 before memmove ");
-  puts(str1);
- 
-  // Copies contents of str2 to str1
-  memmove (str1, str2, 4);
- 
-  puts("\nstr1 after memmove ");
-  puts(str1);
- 
-  return 0;
+	char	str1[50] = "Geeks"; 
+	char	str2[50] = "Quiz"; 
+	
+	puts("str1 before memmove ");
+	puts(str1);
+	// Copies contents of str2 to str1
+	memmove (str1, str2, 4);
+	puts("\nstr1 after memmove ");
+	puts(str1);
+	return 0;
 }
+*/
