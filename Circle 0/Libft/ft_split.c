02898a7f02c30fe4b2fda4bf2260a138170ea652
Hai Anh Le <hle@a2-r5-p1.s19.be>
2023-01-18 13:26:16 +0100

    Add Additional Functions

--- Circle 0/Libft/ft_split.c
+++ Circle 0/Libft/ft_split.c
@@ -6,8 +6,40 @@
 /*   By: hle <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 16:06:37 by hle               #+#    #+#             */
-/*   Updated: 2023/01/13 16:06:38 by hle              ###   ########.fr       */
+/*   Updated: 2023/01/18 10:45:05 by hle              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include "libft.h"
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	n;
+	size_t	i;
+
+	i = 0;
+	n = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			n++;
+		i++;
+	}
+	if (n == 0)
+		return (s);
+	a = (void *)malloc(sizeof (int) * (n + 1))
+	j = 0;
+	k = 0;
+	while (s[j])
+	{
+		if (s[j] == c)
+			
+		j++;
+	}
+	
+}
+/*
+** identifty string s length and number of character "c"
+** if number of c = 0, return string s
+** stuck on create a list, and then creat a new list using malloc. 
 
