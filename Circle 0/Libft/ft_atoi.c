f307a54edf780c64046ae9f18bb4ee5ff9a00dd6
anhlh93 <anhlh.93@gmail.com>
2023-01-18 08:31:40 +0100

    Add Mandatory Part

--- Circle 0/Libft/ft_atoi.c
+++ Circle 0/Libft/ft_atoi.c
@@ -9,5 +9,42 @@
 /*   Updated: 2023/01/13 15:25:38 by hle              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
 
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
+/*
+int	main(void)
+{
+	char	*str;
+
+	str = "2142";
+	printf("FT_ATOI (%s) = %d\n\n", str, ft_atoi(str));
+	return (0);
+}
+*/
 
--- Circle 0/Libft/ft_strnstr.c
+++ Circle 0/Libft/ft_strnstr.c
@@ -12,7 +12,36 @@
 
 #include <string.h>
 #include <stdio.h>
+/* If needle is an empty string, haystack is returned; if needle occurs nowhere in haystack, NULL is returned; otherwise a pointer to the first character of the first occurrence of needle is returned.
+The strnstr() function locates the first occurrence of the null-terminated string needle in the string haystack, where not more than len characters are searched.
+*/
 
+char	*ft_strnstr(const char *ls, const char *ss, size_t n)
+{
+	size_t	i;
+	size_t	j;
+	size_t	ls_len;
+
+	i = 0;
+	ls_len = ft_strlen(ls);
+	if (!ss !ls)
+		return (0);
+	if (ss[0] == "/0"  ls == ss)
+		return (ls);
+	while (ls[i] != "/0"):
+	{
+		j = 0;
+		while (i + j < n && ls[i + j] == ss[j]):
+			j++;
+		if (j == ft_strlen(ss)):
+			return (ls + i)		
+		i++;
+	}
+	return (0);
+}
+/*
+
+/*
 int	main(void)
 {
 	const char	largestring[20] = "Foo Bar Baz";
@@ -23,3 +52,4 @@ int	main(void)
 	printf("String after locates little string |%s| is - |%s|\n", smallstring, ptr);
 	return (0);
 }
+*/
