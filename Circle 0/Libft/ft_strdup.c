3f9092a974a2e8c205a3217bfd5f8f3e1cfe2da3
Hai Anh Le <hle@a2-r5-p1.s19.be>
2023-01-18 10:15:55 +0100

    Add Mandatory functions

--- Circle 0/Libft/ft_strdup.c
+++ Circle 0/Libft/ft_strdup.c
@@ -6,8 +6,36 @@
 /*   By: hle <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 15:25:58 by hle               #+#    #+#             */
-/*   Updated: 2023/01/13 15:25:59 by hle              ###   ########.fr       */
+/*   Updated: 2023/01/18 10:15:24 by hle              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include "libft.h"
+#include <string.h>
+#include <stdio.h>
+// A copy of source is created dynamically
+// and pointer to copy is returned.
 
+char	*ft_strdup(const char *s)
+{
+	char	*new;
+	size_t	size;
+
+	size = ft_strlen(s);
+	new = (char *)malloc(sizeof(char) * (size + 1));
+	if (!new)
+		return (0);
+	ft_memcpy(new, s, size);
+	new[size] = "\0";
+	return (new);
+}
+
+int	main()
+{
+	char	source[] = "GeeksForGeeks";
+	char	*target;
+       
+	target	= strdup(source);
+	printf("%s", target);
+	return (0);
+}
