4f1643603eea2015f8e293380ff01ccff816321d
Hai Anh Le <hle@a2-r5-p1.s19.be>
2023-01-23 17:00:42 +0100

    Add bonus part

--- Circle 00/Libft/ft_lstnew.c
+++ Circle 00/Libft/ft_lstnew.c
@@ -6,10 +6,20 @@
 /*   By: hle <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 16:23:12 by hle               #+#    #+#             */
-/*   Updated: 2023/01/23 16:23:25 by hle              ###   ########.fr       */
+/*   Updated: 2023/01/23 16:59:27 by hle              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
 
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
