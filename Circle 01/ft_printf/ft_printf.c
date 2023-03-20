3df14ffeadb6c467f5a13fd727c5001791ad9383
Hai Anh Le <hle@a2-r5-p1.s19.be>
2023-03-20 11:13:45 +0100

    Update

--- Circle 01/ft_printf/ft_printf.c
+++ Circle 01/ft_printf/ft_printf.c
@@ -12,6 +12,36 @@
 
 #include "ft_printf.h"
 
+int ft_putchar(int c);
+{
+    write(1, &c, 1);
+    return (0);
+}
+
+int ft_conversion(va_list list, char format)
+{
+    int result;
+
+    result = 0;
+    if (char == 'c')
+        result += ft_putchar(va_arg(list, format));
+    else if (char == 's')
+        result += ft_putstr(va_arg(list, format));
+    else if (char == 'p')
+        result += ft_putptr(va_arg(list, format));
+    else if (char == 'd' || char == 'i')
+        result += ft_putnbr(va_arg(list, format));
+    else if (char == 'u')
+        result += ft_put_unsinged_int(va_arg(list, format));
+    else if (char == 'x')
+        result += ft_puthex(va_arg(list, format));
+    else if (char == 'X')
+        result += ft_puthex(va_arg(list, format));
+    else if (char == '%')
+        result += ft_putchar('%');
+    return (result);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	int i;
@@ -19,9 +49,19 @@ int	ft_printf(const char *format, ...)
     int result;
 
     i = 0;
+    result = 0;
     va_start(list, formal);
     while (format[i])
     {
-        
+        if (format[i] == '%')
+        {
+            result += ft_conversion(list, format[i + 1]);
+            i++;
+        }
+        else
+            result += ft_putchar(format[i]);
+        i++;
     }
+    va_end(list);
+    return(result);
 }
---
+++ Circle 01/ft_printf/ft_printf.h
@@ -0,0 +1,10 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include "stdarg.h"
+# include "unistd.h"
+# include "stdlib.h"
+
+int ft_putchar(int c);
+int ft_putstr(char *s);
+
+#endif
