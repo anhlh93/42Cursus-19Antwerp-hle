ef801c7e6d156401dd85d2a6a5055b285105dc2d
Hai Anh Le <hle@fu-r8-p8.s19.be>
2023-03-23 16:02:27 +0100

    Add Mandatory part

---
+++ Circle 01/PRINTF/ft_strlcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hle <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 23:20:08 by hle               #+#    #+#             */
+/*   Updated: 2023/03/23 10:50:33 by hle              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (i < size - 1 && src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	char src[] = "World!";
+	char dest[] = "Hello ";
+	printf("%d | %s", ft_strlcpy(dest, src, 10), dest);
+}
+*/
